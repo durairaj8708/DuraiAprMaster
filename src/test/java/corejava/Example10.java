0cf6f31800999f1c0dd0edd083374c48a9a5e3d4
durai <durai@DESKTOP-J1B52UK.Home>
2022-04-26 21:50:06 +0100

    code done

---
+++ src/test/java/corejava/Example10.java
@@ -0,0 +1,26 @@
+//program10: Array List example
+
+package corejava;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Example10 {
+
+		public static void main(String args[]){  
+		ArrayList<String> list=new ArrayList<String>();//Creating arraylist  
+		list.add("Ravi");//Adding object in arraylist  
+		list.add("Vijay");  
+		list.add("Ravi");  
+		list.add("Ajay");  
+		//Traversing list through Iterator  
+		Iterator itr=list.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+		}  
+		}  
+
+	
+
+ 
